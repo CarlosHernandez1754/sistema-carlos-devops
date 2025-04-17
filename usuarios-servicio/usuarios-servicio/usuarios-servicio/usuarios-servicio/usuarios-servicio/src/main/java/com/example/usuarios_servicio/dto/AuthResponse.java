bf8b02c80f2284ffdd2864e7df4561fba25aecde
CarlosHernandez1754 <127036251+CarlosHernandez1754@users.noreply.github.com>
2025-04-17 18:04:16 -0500

    Add files via upload

---
+++ usuarios-servicio/usuarios-servicio/usuarios-servicio/usuarios-servicio/usuarios-servicio/src/main/java/com/example/usuarios_servicio/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.example.usuarios_servicio.dto;
+
+
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
---
+++ usuarios-servicio/usuarios-servicio/usuarios-servicio/usuarios-servicio/usuarios-servicio/src/main/java/com/example/usuarios_servicio/security/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.example.usuarios_servicio.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "mi_clave_secreta_para_jwt_de_mas_de_32_bytes";
+    private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hora
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey())
+                .build().parseClaimsJws(token).getBody().getSubject();
+    }
+}

8d4093ef683136b78b35afec7606000b6fb87393
CarlosHernandez1754 <127036251+CarlosHernandez1754@users.noreply.github.com>
2025-04-18 10:27:51 -0500

    Delete asignaturas-servicio/asignaturas-servicio directory

--- asignaturas-servicio/asignaturas-servicio/asignaturas-servicio/asignaturas-servicio/src/main/java/com/example/asignaturas_servicio/repository/AsignaturaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.asignaturas_servicio.repository;
-import com.example.asignaturas_servicio.model.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
-}

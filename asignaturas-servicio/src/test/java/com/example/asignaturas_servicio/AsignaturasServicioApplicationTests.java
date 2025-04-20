a0388958ca2b62f36a1be6efab1c17146a4af3e8
CarlosHernandez1754 <127036251+CarlosHernandez1754@users.noreply.github.com>
2025-04-20 09:22:18 -0500

    Update AsignaturasServicioApplicationTests.java

--- asignaturas-servicio/src/test/java/com/example/asignaturas_servicio/AsignaturasServicioApplicationTests.java
+++ asignaturas-servicio/src/test/java/com/example/asignaturas_servicio/AsignaturasServicioApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@ActiveProfiles("test")
 @SpringBootTest
 public class AsignaturasServicioApplicationTests {
 

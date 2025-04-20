dc7f33b2f4f0c6fb58baebdd6790b1bf47e30d17
CarlosHernandez1754 <127036251+CarlosHernandez1754@users.noreply.github.com>
2025-04-20 09:36:31 -0500

    Update MatriculaControllerIntegrationTest.java

--- matriculas-servicio/src/test/java/com/example/matriculas_servicio/MatriculaControllerIntegrationTest.java
+++ matriculas-servicio/src/test/java/com/example/matriculas_servicio/MatriculaControllerIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class MatriculaControllerIntegrationTest {

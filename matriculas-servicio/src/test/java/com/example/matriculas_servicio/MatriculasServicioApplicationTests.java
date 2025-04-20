03b0a165c3ae9e39978b515aaa6b4d60ced9a3ea
CarlosHernandez1754 <127036251+CarlosHernandez1754@users.noreply.github.com>
2025-04-20 11:22:22 -0500

    Update MatriculasServicioApplicationTests.java

--- matriculas-servicio/src/test/java/com/example/matriculas_servicio/MatriculasServicioApplicationTests.java
+++ matriculas-servicio/src/test/java/com/example/matriculas_servicio/MatriculasServicioApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.matriculas_servicio;
-import org.springframework.test.context.ActiveProfiles;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@ActiveProfiles("test")
+
 @SpringBootTest
 class MatriculasServicioApplicationTests {
 
